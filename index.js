82ac3699ab2fbbd7d761558848d25e488c90c762
kronwald <gdegirodon@gmail.com>
2016-02-06 14:40:28 +0100

    add conf param to index

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
+/**
+ * class MasterProcess
+ * @type {[type]}
+ */
 var m = require('./lib/process/MasterProcess');
+module.exports.createMaster = function(conf){return new m(conf)};
+/**
+ * class ChildProcess
+ * @type {[type]}
+ */
 var c = require('./lib/process/ChildProcess');
-module.exports.createMaster = function(){return new m()};
 module.exports.createChild = function(){return new c()};
