97f13583825e45b3f895e9bc88ef656ef137b730
kronwald <gdegirodon@gmail.com>
2016-05-13 09:10:43 +0100

    test event created process

--- test/testMaster.js
+++ test/testMaster.js
@@ -2,16 +2,24 @@ var assert = require('chai').assert;
 
 describe('Master creation', function() {
 	var Master = require('../lib/process/MasterProcess');
-	
+
+	it('should created Master Process with config ', function () {
+		var m = new Master({child_process:[{name:'testChild', process:'testChildProcess', autoRestart:true}]});
+		assert.isTrue(m.start(), 'master  start withconfig');
+	});
+
 	it('should created Master Process without config', function () {
 		var m = new Master();
 		assert.isNotNull(m, 'master not null');
+		assert.isFalse(m.start(), 'master dont start without config');
 	});
 
-	it('should dont start Master Process without config', function () {
-		m = new Master();
-		assert.isFalse(m.start(), 'master start false without config');
+	it('should dispatch process created after process child created', function (done) {
+		var m = new Master({child_process:[{name:'testChild', process:'testChildProcess', autoRestart:true}]});
+		m.on(m.ON_PROCESS_CREATED, function(proc){
+			done()
+		})
+		m.start()
 	});
-
 	
 });
