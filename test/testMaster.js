2f82118c3f2577a5c235f0af3ca7efdcaeebac12
kronwald <gdegirodon@gmail.com>
2016-05-13 11:28:38 +0100

    update travis to node 6.1

--- test/testMaster.js
+++ test/testMaster.js
@@ -5,7 +5,7 @@ describe('Master creation', function() {
 
 	it('should created Master Process with config ', function () {
 		var m = new Master({child_process:[{name:'testChild', process:'testChildProcess', autoRestart:true}]});
-		assert.isTrue(m.start(), 'master  start withconfig');
+		assert.isTrue(m.start(), 'master  start with config');
 	});
 
 	it('should created Master Process without config', function () {
@@ -14,12 +14,30 @@ describe('Master creation', function() {
 		assert.isFalse(m.start(), 'master dont start without config');
 	});
 
-	it('should dispatch process created after process child created', function (done) {
+/*	it('should dispatch process created after process child created', function (done) {
 		var m = new Master({child_process:[{name:'testChild', process:'testChildProcess', autoRestart:true}]});
 		m.on(m.ON_PROCESS_CREATED, function(proc){
 			done()
 		})
 		m.start()
+	});*/
+
+});
+
+
+describe('Master functionnality', function() {
+	var Master = require('../lib/process/MasterProcess');
+	var m = new Master({child_process:[{name:'testChild', process:'testChildProcess', autoRestart:true}]});
+
+	it('should dispatch process created after process child created', function (done) {
+		m.on(m.ON_PROCESS_CREATED, function(proc){
+			done()
+		})
+		m.start()
 	});
-	
+
+	it('should list all process list', function () {
+		assert.isObject(m.processList,'process list is object');
+	});
+
 });
