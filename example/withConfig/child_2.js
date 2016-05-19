2720dbd8b5a2da0b8a519df89259fb620f0f8180
kronwald <gdegirodon@gmail.com>
2016-05-19 11:43:02 +0100

    clean example

--- example/withConfig/child_2.js
+++ example/withConfig/child_2.js
@@ -2,5 +2,3 @@ var child = require('../../index').createChild();
 child.on('message', function(data){
 		console.log(this.processName,' received data :',data );
 });
-//child.sendMessage({to:'child1', content:'hello from 1'});
-//setInterval(child.sendMessage.bind(child, {to:'child1', content:'hello from 2'}),5000);
