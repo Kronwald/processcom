7fd8ad37c9f5f0cbc14bd5933b8eda9c3034144e
Gauthier <dgpgdev@gmail.com>
2016-08-24 18:27:49 +0200

    fix unit test master

--- test/testMaster.js
+++ test/testMaster.js
@@ -2,31 +2,36 @@ var assert = require('chai').assert;
 var Master = require('../lib/process/MasterProcess');
 describe('Master', function() {
     describe('create master process', function() {
-        var m = new Master({
-            child_process: [{
-                name: 'test1',
-                process: 'testChildProcess',
-                autoRestart: true
-            }, {
-                name: 'test2',
-                process: 'testChildProcess',
-                autoRestart: true
-            }]
-        });
-        it('should created Master Process with config ', function() {
+        it('should created process from config ', function(done) {
+            let m = new Master({
+                child_process: [{
+                    name: 'test1',
+                    process: 'test/testChild',
+                    autoRestart: true
+                }, {
+                    name: 'test2',
+                    process: 'test/testChild',
+                    autoRestart: true
+                }]
+            });
+            m.on(m.ON_ALL_PROCESS_CREATED, function(proc) {
+                assert.equal(m.processList, proc, 'all process are created')
+                assert.equal(m.totalCreatedProcess, 2);
+                done()
+            })
             assert.isTrue(m.start(), 'master  start with config');
         });
-
-				it('should dispatch process created after ALL process child created', function(done) {
-						m.on(m.ON_ALL_PROCESS_CREATED, function(proc) {
-								assert.equal(m.processList, proc,'all process are created')
-								assert.equal(m.totalCreatedProcess, 2);
-								done()
-						})
-						m.start()
-				});
-        after(function() {
-            m.killAllProcess()
+        it('should create process on fly', function(done) {
+            let m = new Master();
+            m.on(m.ON_ALL_PROCESS_CREATED, function(proc) {
+                assert.equal(m.processList, proc, 'all process are created')
+                assert.equal(m.totalCreatedProcess, 1);
+                done()
+            })
+            m.createProcess({
+                name: 'test',
+                process: 'test/testChild'
+            });
         })
     })
 });
