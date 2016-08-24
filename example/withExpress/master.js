de72e82d2741fb7abaa3336182944fbab826cf61
Gauthier <dgpgdev@gmail.com>
2016-08-24 17:45:38 +0200

    fix example express and add error event process already exist

--- example/withExpress/master.js
+++ example/withExpress/master.js
@@ -1,25 +1,9 @@
 'use strict'
-//var config = require('./config.json');
 var master = require('../../index').createMaster();
 var express = require('express');
 
-//listen process created
-/*master.on(master.ON_PROCESS_CREATED, function(proc) {
-  if(requestStack[proc.data.name]!== undefined){
-    requestStack[proc.data.name].send('created process')
-    delete requestStack[proc.data.name];
-  }
-})*/
-
-//listen all process created
-master.on(master.ON_ALL_PROCESS_CREATED, function(proc) {
-    console.log('All process in config file created');
-})
-
 //start master
 master.start();
-
-
 
 /////////////////
 // EXPRESS INIT
@@ -30,21 +14,44 @@ var bodyParser = require('body-parser')
 // parse application/json
 app.use(bodyParser.json())
 
-const requestStack = {};
+/***********************************************
+CREATE PROCESS
 
+http request  : http://localhost:8080/process/create
+
+body : [{
+            "name": "first",
+            "process": "child_1",
+            "autoRestart": true
+        }, {
+            "name": "second",
+            "process": "child_1",
+            "autoRestart": true
+        }
+    ]
+
+Content Type : application/json
+
+***********************************************/
 
 app.post('/process/create', function(req, res) {
-    /*  requestStack[req.body.name] = res;
-
-      next();*/
-    master.on(master.ON_PROCESS_CREATED, function(proc) {
-        res.send('process created');
+  master.once(master.ON_ERROR, function(error) {
+      res.send(error.message);
+      res.end()
+  })
+    master.once(master.ON_ALL_PROCESS_CREATED, function(proc) {
+        res.send('all process created : current process '+ this.totalCreatedProcess);
         res.end()
-        console.log(this)
     })
     master.createProcess(req.body);
 });
 
+/***********************************************
+Kill PROCESS
+http request : http://localhost:8080/process/kill/first (kill process with name first)
+
+http request : http://localhost:8080/process/kill/all (kill all child process)
+***********************************************/
 app.post('/process/kill/:id', function(req, res) {
         if (req.params.id === undefined) {
             res.send('error');
@@ -55,18 +62,13 @@ app.post('/process/kill/:id', function(req, res) {
             master.killAllProcess();
             res.send('all process killed');
             res.end();
-            //  return
-        } else {
-            master.killProcess(id);
-            res.send('process ' + id + 'killed');
-            res.end();
+            return
         }
-
+        master.killProcess(req.params.id);
+        res.send('process ' + req.params.id + ' killed');
+        res.end();
     }
-
 );
 
-
-
-
+//http port listen
 app.listen(8080);
